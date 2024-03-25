a05b81d1a0e87ad2482d3d884c938983fa1a89a2
JoaoGabrielVS19 <joaogabrielsilvaqdi@gmail.com>
2024-03-25 10:46:17 -0300

    chore: created user  routes

---
+++ src/controller/userController.go
@@ -0,0 +1,11 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetAll(c *gin.Context)     {}
+func GetByEmail(c *gin.Context) {}
+func Create(c *gin.Context)     {}
+func Update(c *gin.Context)     {}
+func Delete(c *gin.Context)     {}

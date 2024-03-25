c40ea05984b3e0b6498122a4b833873658a6ca19
JoaoGabrielVS19 <joaogabrielsilvaqdi@gmail.com>
2024-03-25 11:56:42 -0300

    feat: implemented user create endpoint and error validation

--- src/controller/userController.go
+++ src/controller/userController.go
@@ -1,11 +1,30 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joaodrts/Go.Crud.Api/src/configuration/validation"
+	"github.com/joaodrts/Go.Crud.Api/src/controller/model/request"
 )
 
 func GetAll(c *gin.Context)     {}
 func GetByEmail(c *gin.Context) {}
-func Create(c *gin.Context)     {}
-func Update(c *gin.Context)     {}
-func Delete(c *gin.Context)     {}
+
+func Create(c *gin.Context) {
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+
+		restErr := validation.ValidateUserError(err)
+		c.JSON(restErr.Code, restErr)
+
+		return
+	}
+
+	fmt.Println(userRequest)
+}
+
+func Update(c *gin.Context) {}
+func Delete(c *gin.Context) {}
